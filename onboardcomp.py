1950cb7acff504d343266a0d54e48e7ae5717a7a
Miguel <miguelmaidebezares@gmail.com>
2024-02-06 21:42:33 +0100

    Great changes on class __init__ battery instantiation and change_processs_data formula created

--- onboardcomp.py
+++ onboardcomp.py
@@ -1,13 +1,23 @@
-class BoardComp:
+class BoardComputer:
 
     def __init__(self, status="off"):
+        self.battery = Battery()
         self.status = status
         self.speed = 0
         self.move = False
         self.__manufacturer = "Mike Studios"
         self.__serialno = 1234567
         self.__software = "Studio 1.0"
-        self.process_data = {}
+        self.process_data = {
+            "Speed": 0,
+            "Mileage": None,
+            "Location": None
+        }
+
+    def __add__(self, computer_object):
+        """Computer object can either be HardwareControl or ProcessUnit"""
+        if type(computer_object).__name__ == "ProcessUnit":
+            self.process_data["Location"] = computer_object.location
 
     def accelerate(self, value=5):
         self.move = True
@@ -19,14 +29,22 @@ class BoardComp:
         else:
             self.accelerate(force_input)
 
-    def run_mode(self, process_unit, chassis):
+    def change_process_data(self, process_unit, hardware_control):
+        self.process_data["Speed"] = (process_unit.speed + hardware_control.speed)/2  #average speed coming from sensors and mechanical
+        self + process_unit
+
+
+
+    def run_mode(self):
         while True:
-            if self.status is "off":
+            if self.status.lower() is "off":
                 print("System OFF")
                 break
-            elif self.status is "on":
+            elif self.status.lower() is "on":
+                print("System ON")
                 while True:
+                    console = ProcessUnit()
                     if self.move is True:
-                        print(f"Moving... position coordinates {process_unit.location}")
+                        print(f"Moving... position coordinates {console.location}")
                     elif self.move is False:
-                        print(f"Vehicle stopped at {process_unit.location}")
+                        print(f"Vehicle stopped at {console.location}")
