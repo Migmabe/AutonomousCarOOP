613e71ddad32aac0794c31178e5e98d8cb2e75a9
Miguel <miguelmaidebezares@gmail.com>
2024-02-06 21:43:01 +0100

    Unfinished and coords dataset created

--- processing.py
+++ processing.py
@@ -1,45 +1,57 @@
 # Here all the modules connected to the Process Unit class
+from onboardcomp import BoardComputer
+from csv import reader
 
-class ProcessUnit:
+#######################################################
+"""Here's the data that would supposedly come from sensors in real time"""
+coord = []
+with open("files/x_y.csv") as coordinates:
+    csv_read = reader(coordinates, delimiter=",")
+    next(coordinates)  # skip first titles row
+    for x, y in csv_read:
+        coord.append([x, y])
 
-    def __init__(self):
+
+#######################################################
+
+class ProcessUnit(BoardComputer):
+
+    def __init__(self, imu_object):
+        super().__init__()
         self.error_list = []
         self.error_count = 0
+        self.imu = imu_object
 
-    """def running(self):
-        while True:
-            if"""
+    def __setattr__(self, key, value):
+        print(f"Setting {key} to {value}")
+        super().__setattr__(key, value)
 
-
-
+    @property
+    def location(self):
+        """do average between get_position and get_location"""
+        if self.imu.position is not None and
 
 
 class NetworkAntenna:
     pass
 
 
-class GNS:
-    pass
+class GNSS:
+
+    def __init__(self, latitude=0, longitude=0):
 
 
 class ObstacleDetection:
     pass
 
 
-class IMU(ProcessUnit):
+class IMU:
     """With acceleration over time and orientation, it can get its position in coordinates"""
 
-    orientations = {
-        "north": 90,
-        "west": 180,
-        "south": 270,
-        "east": 0
-    }
-
-    def __init__(self, acceleration):
-        super().__init__()
+    def __init__(self, acceleration=0):
         self.acceleration = acceleration
         self._orientation = None
+        self._position = None
 
     @property
     def orientation(self):
@@ -47,20 +59,28 @@ class IMU(ProcessUnit):
 
     @orientation.setter
     def orientation(self, new_value):
-        if type(new_value) is str:
-            self.error_log("Orientation must be a cardinal direction or a number")
-
-    def translate(self, value):
-        self.orientation = self.orientations[value]
-        return self.orientation
-
-        print("Orientation...")
-        if new_value.lower() in ["north", "south", "east", "west"]:
-            self.translate(new_value.lower())
-        elif new_value.isnumeric():
-            self.orientation = new_value
+        if type(new_value) is not int or float:
+            self._orientation.cardinal_directions(new_value)
         else:
-            raise ValueError("Invalid input")
+            self._orientation = new_value
+
+    def cardinal_directions(self, direction):
+        if direction.lower() == "north":
+            self._orientation = 90
+        elif direction.lower() == "south":
+            self._orientation = 270
+        elif direction.lower() == "east":
+            self._orientation = 0
+        elif direction.lower() == "west":
+            self._orientation = 180
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, new_value):
+        self._position = new_value
 
 
 class LightSensor:
